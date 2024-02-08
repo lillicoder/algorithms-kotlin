19166e9dd36aeef371562ccfeb79f7c286161285
lillicoder <scott@lillicoder.com>
2024-02-08 14:08:34 -0500

    Implementation of insertion sort and unit tests

---
+++ sort/src/main/kotlin/com/lillicoder/algorithms/sort/InsertionSort.kt
@@ -0,0 +1,20 @@
+package com.lillicoder.algorithms.sort
+
+/**
+ * Implementation of [Insertion Sort](https://en.wikipedia.org/wiki/Insertion_sort).
+ */
+class InsertionSort : Sort {
+    override fun <T : Comparable<T>> sort(list: List<T>) = list.toMutableList().apply { sort(this) }
+
+    private fun <T : Comparable<T>> sort(list: MutableList<T>) {
+        if (list.isEmpty()) return
+
+        for (index in 1..<list.size) {
+            var currentIndex = index
+            while (currentIndex > 0 && list[currentIndex - 1] > list[currentIndex]) {
+                list.swap(currentIndex, currentIndex - 1)
+                currentIndex--
+            }
+        }
+    }
+}
---
+++ sort/src/test/kotlin/com/lillicoder/algorithms/sort/InsertionSortTest.kt
@@ -0,0 +1,34 @@
+package com.lillicoder.algorithms.sort
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+internal class InsertionSortTest {
+    @Test
+    fun `Insertion sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = InsertionSort().sort(input)
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Insertion sort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = InsertionSort().sort(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Insertion sort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = InsertionSort().sort(input)
+
+        assertEquals(expected, actual)
+    }
+}
