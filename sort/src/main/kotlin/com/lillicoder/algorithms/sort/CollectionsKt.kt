b960c5c52e9f590e6143a51ffc8928cc5a1e3922
lillicoder <scott@lillicoder.com>
2025-02-27 13:24:57 -0500

    Reworked all sorts to be extension functions on existing collection types

---
+++ sort/src/main/kotlin/com/lillicoder/algorithms/sort/CollectionsKt.kt
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2025 Scott Weeden-Moody
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this project except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lillicoder.algorithms.sort
+
+import com.lillicoder.algorithms.collections.swap
+import com.lillicoder.algorithms.trees.ArrayBinaryHeap
+import com.lillicoder.algorithms.trees.BinarySearchTree
+
+/***************
+ * Bubble Sort *
+ ***************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [bubble sort](https://en.wikipedia.org/wiki/Bubble_sort).
+ */
+fun <T : Comparable<T>> List<T>.bubbleSorted() = toMutableList().apply { bubbleSort() }
+
+/**
+ * Sorts elements in this list in-place according to their natural sort order
+ * using [bubble sort](https://en.wikipedia.org/wiki/Bubble_sort).
+ */
+fun <T : Comparable<T>> MutableList<T>.bubbleSort() {
+    if (isEmpty()) return
+
+    var didSwap: Boolean
+    do {
+        didSwap = false
+        for (index in 1..<size) {
+            val previous = index - 1
+            if (get(previous) > get(index)) {
+                swap(previous, index)
+                didSwap = true
+            }
+        }
+    } while (didSwap)
+}
+
+/************
+ * Heapsort *
+ ************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [Heapsort](https://en.wikipedia.org/wiki/Heapsort).
+ */
+fun <T : Comparable<T>> Collection<T>.heapSorted() = ArrayBinaryHeap(this).sorted().toList()
+
+/******************
+ * Insertion Sort *
+ ******************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [insertion sort](https://en.wikipedia.org/wiki/Insertion_sort).
+ */
+fun <T : Comparable<T>> List<T>.insertionSorted() = toMutableList().apply { insertionSort() }
+
+/**
+ * Sorts elements in this list in-place according to their natural sort order
+ * using [insertion sort](https://en.wikipedia.org/wiki/Insertion_sort).
+ */
+fun <T : Comparable<T>> MutableList<T>.insertionSort() {
+    if (isEmpty()) return
+
+    for (index in 1..<size) {
+        var currentIndex = index
+        while (currentIndex > 0 && get(currentIndex - 1) > get(currentIndex)) {
+            swap(currentIndex, currentIndex - 1)
+            currentIndex--
+        }
+    }
+}
+
+/**************
+ * Merge Sort *
+ **************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [merge sort](https://en.wikipedia.org/wiki/Merge_sort).
+ */
+fun <T : Comparable<T>> List<T>.mergeSorted(): List<T> {
+    if (size <= 1) return this
+
+    val middle = size ushr 1
+    val left = subList(0, middle).mergeSorted()
+    val right = subList(middle, size).mergeSorted()
+    return left.merge(right)
+}
+
+/**
+ * Merges this list with the given list while keeping elements in order.
+ * @param other List to merge.
+ * @return Merged list.
+ */
+private fun <T: Comparable<T>> List<T>.merge(other: List<T>): List<T> {
+    val merged = mutableListOf<T>()
+
+    var leftPosition = 0
+    var rightPosition = 0
+
+    for (index in 0..<size + other.size) {
+        val left = getOrNull(leftPosition)
+        val right = other.getOrNull(rightPosition)
+
+        if (left != null && right != null) {
+            // Valid elements, put them in order
+            when (left < right) {
+                true -> {
+                    merged.add(left)
+                    leftPosition++
+                }
+                false -> {
+                    merged.add(right)
+                    rightPosition++
+                }
+            }
+        } else if (left != null) {
+            // Invalid right element, use left
+            merged.add(left)
+            leftPosition++
+        } else if (right != null) {
+            // Invalid left element, use right
+            merged.add(right)
+            rightPosition++
+        } else {
+            // Something has gone awry
+            throw IndexOutOfBoundsException(
+                "Index out of bounds in merge. [leftPosition=$leftPosition, rightPosition=$rightPosition]",
+            )
+        }
+    }
+
+    return merged
+}
+
+/*************
+ * Quicksort *
+ *************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [quicksort](https://en.wikipedia.org/wiki/Quicksort).
+ */
+fun <T : Comparable<T>> List<T>.quicksorted() = toMutableList().apply { quicksort() }
+
+/**
+ * Sorts elements in this list in-place according to their natural sort order
+ * using [quicksort](https://en.wikipedia.org/wiki/Quicksort).
+ */
+fun <T: Comparable<T>> MutableList<T>.quicksort() {
+    if (isEmpty()) return
+
+    val pivot = partition()
+    subList(0, pivot).quicksort()
+    subList(pivot + 1, size).quicksort()
+}
+
+/**
+ * Partitions this list and returns the pivot index.
+ * @return Pivot index.
+ */
+private fun <T : Comparable<T>> MutableList<T>.partition(): Int {
+    val pivot = last()
+
+    var pivotIndex = -1
+    forEachIndexed { index, element ->
+        if (element < pivot) {
+            pivotIndex++
+            swap(pivotIndex, index)
+        }
+    }
+
+    // Move pivot to final position
+    pivotIndex++
+    swap(pivotIndex, size - 1)
+
+    return pivotIndex
+}
+
+/******************
+ * Selection Sort *
+ ******************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [selection sort](https://en.wikipedia.org/wiki/Selection_sort).
+ */
+fun <T : Comparable<T>> List<T>.selectionSorted() = toMutableList().apply { selectionSort() }
+
+/**
+ * Sorts elements in this list in-place according to their natural sort order
+ * using [selection sort](https://en.wikipedia.org/wiki/Selection_sort).
+ */
+fun <T : Comparable<T>> MutableList<T>.selectionSort() {
+    for (index in 0..<size) {
+        var minIndex = index // Find next minimum, start at index
+        for (nextIndex in index + 1..<size) {
+            if (get(nextIndex) < get(minIndex)) {
+                minIndex = nextIndex // New minimum value
+                continue
+            }
+        }
+
+        if (minIndex != index) swap(index, minIndex)
+    }
+}
+
+/*************
+ * Tree Sort *
+ *************/
+
+/**
+ * Returns a list of all elements sorted according to their natural sort order
+ * using [tree sort](https://en.wikipedia.org/wiki/Tree_sort).
+ */
+fun <T : Comparable<T>> Collection<T>.treeSorted() = BinarySearchTree(this).sorted()
---
+++ sort/src/test/kotlin/com/lillicoder/algorithms/sort/CollectionKtTest.kt
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2025 Scott Weeden-Moody
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this project except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lillicoder.algorithms.sort
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+/**
+ * Unit tests for sort extensions.
+ */
+internal class CollectionKtTest {
+    /***************
+     * Bubble Sort *
+     ***************/
+
+    @Test
+    fun `Bubble sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.bubbleSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Bubble sort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.bubbleSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Bubble sort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.bubbleSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /************
+     * Heapsort *
+     ************/
+
+    @Test
+    fun `Heapsort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.heapSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Heapsort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.heapSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Heapsort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.heapSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /******************
+     * Insertion Sort *
+     ******************/
+
+    @Test
+    fun `Insertion sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.insertionSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Insertion sort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.insertionSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Insertion sort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.insertionSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /**************
+     * Merge Sort *
+     **************/
+
+    @Test
+    fun `Merge sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.mergeSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Merge sort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.mergeSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Merge sort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.mergeSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /*************
+     * Quicksort *
+     *************/
+
+    @Test
+    fun `Quicksort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.quicksorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Quicksort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.quicksorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Quicksort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.quicksorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /******************
+     * Selection Sort *
+     ******************/
+
+    @Test
+    fun `Selection sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.selectionSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Selection sort matches known concrete sorted list`() {
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.selectionSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Selection sort matches Kotlin SDK for a random list`() {
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }
+        val expected = input.sorted()
+        val actual = input.selectionSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    /*************
+     * Tree Sort *
+     *************/
+
+    @Test
+    fun `Tree sort handles empty list`() {
+        val input = mutableListOf<Int>()
+        val actual = input.treeSorted()
+
+        assertTrue(actual.isEmpty())
+    }
+
+    @Test
+    fun `Tree sort matches known concrete sorted list`() {
+        // Binary trees don't allow duplicates, so only check for unique ordered values
+        val input = listOf("delta", "tau", "zeta", "alpha", "gamma", "alpha", "beta", "epsilon")
+        val expected = listOf("alpha", "beta", "delta", "epsilon", "gamma", "tau", "zeta")
+        val actual = input.treeSorted()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Tree sort matches Kotlin SDK for a random list`() {
+        // Binary trees don't allow duplicates, so only check for unique ordered values
+        val input = IntRange(1, 100).map { Random.Default.nextInt() }.distinct()
+        val expected = input.sorted()
+        val actual = input.treeSorted()
+
+        assertEquals(expected, actual)
+    }
+}
